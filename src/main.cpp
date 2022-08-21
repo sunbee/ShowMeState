abc00182ef66b2cf1b63cbe3cc218293ebbad490
sunbee <sanjay.bhatikar@gmail.com>
2022-08-21 00:21:25 -0500

    Added touch sense

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,19 @@
 #include <SPI.h>
 #include <TFT_eSPI.h>
 
+#include "LittleFS.h"
+
 TFT_eSPI _tft = TFT_eSPI();
+
+// This is the file name used to store the calibration data
+// You can change this to create new calibration files.
+// The LittleFS file name must start with "/".
+#define CALIBRATION_FILE "/calibration"
+
+// Set REPEAT_CAL to true instead of false to run calibration
+// again, otherwise it will only be done once.
+// Repeat calibration if you change the screen rotation.
+#define REPEAT_CAL false
 
 // Numeric display box size and location
 #define DISP_X 1
@@ -25,6 +37,15 @@ TFT_eSPI _tft = TFT_eSPI();
 #define LABEL1_FONT &FreeSansOblique12pt7b // Key label font 1
 #define LABEL2_FONT &FreeSansBold12pt7b    // Key label font 2
 
+// Number length, buffer for storing it and character index
+#define NUM_LEN 12
+char numberBuffer[NUM_LEN + 1] = "";
+uint8_t numberIndex = 0;
+
+// We have a status line for messages
+#define STATUS_X 120 // Centred on this
+#define STATUS_Y 65
+
 // Create 15 keys for the keypad
 char keyLabel[15][5] = {"New", "Del", "Send", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "0", "#" };
 uint16_t keyColor[15] = {TFT_RED, TFT_DARKGREY, TFT_DARKGREEN,
@@ -35,6 +56,90 @@ uint16_t keyColor[15] = {TFT_RED, TFT_DARKGREY, TFT_DARKGREEN,
                         };
 
 TFT_eSPI_Button key[15];
+
+void littleNamaste() {
+  if (!LittleFS.begin()) {
+    Serial.println("Mounted no LittleFS!");
+    return;
+  }
+
+  File file = LittleFS.open("/calibration", "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.println("File Content: ");
+  while (file.available()) {
+    Serial.write(file.read());
+  }
+  file.close();
+}
+
+void touch_calibrate()
+{
+  uint16_t calData[5];
+  uint8_t calDataOK = 0;
+
+  // Check file system exists
+  if (!LittleFS.begin()) {
+    Serial.println("Formating file system");
+    LittleFS.format();
+    LittleFS.begin();
+  }
+
+  // check if calibration file exists and size is correct
+  if (LittleFS.exists(CALIBRATION_FILE)) {
+    if (REPEAT_CAL)
+    {
+      // Delete if we want to re-calibrate
+      LittleFS.remove(CALIBRATION_FILE);
+    }
+    else
+    {
+      File f = LittleFS.open(CALIBRATION_FILE, "r");
+      if (f) {
+        if (f.readBytes((char *)calData, 14) == 14)
+          calDataOK = 1;
+        f.close();
+      }
+    }
+  }
+
+  if (calDataOK && !REPEAT_CAL) {
+    // calibration data valid
+    _tft.setTouch(calData);
+  } else {
+    // data not valid so recalibrate
+    _tft.fillScreen(TFT_BLACK);
+    _tft.setCursor(20, 0);
+    _tft.setTextFont(2);
+    _tft.setTextSize(1);
+    _tft.setTextColor(TFT_WHITE, TFT_BLACK);
+
+    _tft.println("Touch corners as indicated");
+
+    _tft.setTextFont(1);
+    _tft.println();
+
+    if (REPEAT_CAL) {
+      _tft.setTextColor(TFT_RED, TFT_BLACK);
+      _tft.println("Set REPEAT_CAL to false to stop this running again!");
+    }
+
+    _tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+
+    _tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    _tft.println("Calibration complete!");
+
+    // store data
+    File f = LittleFS.open(CALIBRATION_FILE, "w");
+    if (f) {
+      f.write((const unsigned char *)calData, 14);
+      f.close();
+    }
+  }
+}
 
 void drawKeypad()
 {
@@ -55,6 +160,17 @@ void drawKeypad()
   }
 }
 
+// Print something in the mini status bar
+void status(const char *msg) {
+  _tft.setTextPadding(240);
+  //tft.setCursor(STATUS_X, STATUS_Y);
+  _tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
+  _tft.setTextFont(0);
+  _tft.setTextDatum(TC_DATUM);
+  _tft.setTextSize(1);
+  _tft.drawString(msg, STATUS_X, STATUS_Y);
+}
+
 void setup() {
   // put your setup code here, to run once:
     // Use serial port
@@ -67,8 +183,8 @@ void setup() {
   //_tft.setRotation(1);
 
   // Calibrate the touch screen and retrieve the scaling factors
-  //touch_calibrate();
-
+  touch_calibrate();
+  //littleNamaste();
 
   // Clear the screen
   _tft.fillScreen(TFT_BLACK);
@@ -86,5 +202,77 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+    uint16_t t_x = 0, t_y = 0; // To store the touch coordinates
+
+  // Pressed will be set true is there is a valid touch on the screen
+  bool pressed = _tft.getTouch(&t_x, &t_y);
+
+  // / Check if any key coordinate boxes contain the touch coordinates
+  for (uint8_t b = 0; b < 15; b++) {
+    if (pressed && key[b].contains(t_x, t_y)) {
+      key[b].press(true);  // tell the button it is pressed
+    } else {
+      key[b].press(false);  // tell the button it is NOT pressed
+    }
+  }
+
+  // Check if any key has changed state
+  for (uint8_t b = 0; b < 15; b++) {
+
+    if (b < 3) _tft.setFreeFont(LABEL1_FONT);
+    else _tft.setFreeFont(LABEL2_FONT);
+
+    if (key[b].justReleased()) key[b].drawButton();     // draw normal
+
+    if (key[b].justPressed()) {
+      key[b].drawButton(true);  // draw invert
+
+      // if a numberpad button, append the relevant # to the numberBuffer
+      if (b >= 3) {
+        if (numberIndex < NUM_LEN) {
+          numberBuffer[numberIndex] = keyLabel[b][0];
+          numberIndex++;
+          numberBuffer[numberIndex] = 0; // zero terminate
+        }
+        status(""); // Clear the old status
+      }
+
+      // Del button, so delete last char
+      if (b == 1) {
+        numberBuffer[numberIndex] = 0;
+        if (numberIndex > 0) {
+          numberIndex--;
+          numberBuffer[numberIndex] = 0;//' ';
+        }
+        status(""); // Clear the old status
+      }
+
+      if (b == 2) {
+        status("Sent value to serial port");
+        Serial.println(numberBuffer);
+      }
+      // we dont really check that the text field makes sense
+      // just try to call
+      if (b == 0) {
+        status("Value cleared");
+        numberIndex = 0; // Reset index to 0
+        numberBuffer[numberIndex] = 0; // Place null in buffer
+      }
+
+      // Update the number display field
+      _tft.setTextDatum(TL_DATUM);        // Use top left corner as text coord datum
+      _tft.setFreeFont(&FreeSans18pt7b);  // Choose a nicefont that fits box
+      _tft.setTextColor(DISP_TCOLOR);     // Set the font colour
+
+      // Draw the string, the value returned is the width in pixels
+      int xwidth = _tft.drawString(numberBuffer, DISP_X + 4, DISP_Y + 12);
+
+      // Now cover up the rest of the line up by drawing a black rectangle.  No flicker this way
+      // but it will not work with italic or oblique fonts due to character overlap.
+      _tft.fillRect(DISP_X + 4 + xwidth, DISP_Y + 1, DISP_W - xwidth - 5, DISP_H - 2, TFT_BLACK);
+
+      delay(10); // UI debouncing
+    }
+  }
 }
 
